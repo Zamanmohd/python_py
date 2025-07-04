043237c3aa9e4e25cde2ca1ee687ef017a9a49e8
unknown <zamanzid151@gmail.com>
2025-07-04 13:31:27 +0530

    conditional_statements is updated

--- conditional_statements.py
+++ conditional_statements.py
@@ -5,4 +5,5 @@ if age>=18:
     print("{} can apply for pan card".format(name))
 else:
     print("{} is eligible to apply for pan card".format(name))
-    print("{} you can only apply when you become 18 years old")
+    print("{} you can apply become  old".format(name))
+    
