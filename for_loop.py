244bb9d26befc050df1b1a141b450ec0a707dfd6
unknown <zamanzid151@gmail.com>
2025-07-10 14:12:13 +0530

    for loop and range

---
+++ for_loop.py
@@ -0,0 +1,31 @@
+my_list=[12,24,36,48,52]
+
+for value in my_list:
+    print(value,end=" ")
+    
+print("")
+  
+my_tuple=(12,24,36,48,52)    
+
+for tuple in my_tuple:
+    print(tuple,end=" ")
+
+print("")
+
+my_set={12,24,36,48,52}
+
+for set in my_set:
+    print(set,end=" ") 
+     
+print("")
+
+    
+my_dict={"name":"zaman","age":"21","adress":"kerala"}
+""""
+for dict in my_dict:
+    print(dict,end=" ")
+print(my_dict.items())
+"""
+for c,d in my_dict.items():
+    print(c,d)
+      
---
+++ range.py
@@ -0,0 +1,14 @@
+print(list(range(3,21,11)))
+
+for a in range(12):
+    print(a*2)
+
+"""num=int(input("enter a number:"))
+for a in range(num):
+    print(a*4)"""
+    
+for num in range(1,17):
+     if num%2!=0:
+         print(num)
+        
+        
