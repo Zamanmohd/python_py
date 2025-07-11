8078cb9da222300e6b0e4cf554c6eda1bdf7d336
unknown <zamanzid151@gmail.com>
2025-07-11 13:49:45 +0530

    loop task

---
+++ task.py
@@ -0,0 +1,22 @@
+"""fruits=["apple","grape","orange","anar"]
+for o in fruits:
+ if o.lower().startswith("o"):
+    print(o)"""
+"""
+for w in fruits:
+    if w.lower()[0]=='a':
+     print(w)"""
+     
+"""for num in range(1,15):
+    if num==4:
+     continue
+    print(num)"""
+
+"""for num in range(1,15):
+    if num==4:
+        break
+    print(num)"""
+    
+for r in range(1,4):
+ for j in range(1, r+1):
+    print(j,end=" ")
