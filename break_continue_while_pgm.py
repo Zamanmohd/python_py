2ca29925de6631505ac5e5791006872bf945e582
unknown <zamanzid151@gmail.com>
2025-07-14 14:15:43 +0530

    while loop

---
+++ break_continue_while_pgm.py
@@ -0,0 +1,7 @@
+"""count=1
+while count<=10:
+
+    if count==8:
+    break
+    print(count,end=" ")
+    count+=1"""
--- task.py
+++ task.py
@@ -16,7 +16,21 @@ for w in fruits:
     if num==4:
         break
     print(num)"""
-    
+"""    
 for r in range(1,4):
  for j in range(1, r+1):
-    print(j,end=" ")
+    print(j,end="")"""
+    
+"""for z in range(2,4):
+  for m in range(2,z+2):
+    print(m,end=(" "))"""
+    
+"""for num in range(1,13):
+   if num==7:
+      break
+   print(num)
+   """
+"""for num in range(1,11):
+  if num==5:
+      continue
+  print(num)"""
